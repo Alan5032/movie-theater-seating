0ef1224939c7cb76bdd15a4147076b39b51dbf9e
Alan5032 <46549617+Alan5032@users.noreply.github.com>
2022-03-21 14:45:34 -0700

    eliminated magic numbers

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,11 @@ public class Main {
     private static final int NUM_COLUMNS = 20;  // The number of columns in the seating arrange of a movie theater
     private static final int BUFFER = 3;  // The horizontal buffer space required between reservation groups
     private static final int UPPERCASE_A_ASCII = 65;  // The ASCII value of 'A'
+    private static final int SIZE_INDEX = 1;  // Index of the size of a reservation
+    private static final int IDENTIFIER_INDEX = 0;  // Index of the reservation identifier
+    private static final int ROW_INDEX = 0;  // Index for row of assigned seats
+    private static final int STARTING_COLUMN_INDEX = 1;  // Index for starting column of assigned seats
+    private static final int ENDING_COLUMN_INDEX = 2;  // Index for ending column of assigned seats
 
     public static void main(String[] args) {
         Theater theater = new Theater(NUM_ROWS, NUM_COLUMNS, BUFFER);  // Theater for the reservations
@@ -35,24 +40,24 @@ public class Main {
             while (file.hasNextLine()) {
                 // Split the reservation into its identifier and number of people
                 String[] reservation = file.nextLine().split("\\s+");
-                int people = Integer.parseInt(reservation[1]);  // Number of people in this reservation
+                int people = Integer.parseInt(reservation[SIZE_INDEX]);  // Number of people in this reservation
                 // Get all assigned seats for this reservation
                 List<Integer[]> allReservedSeats = theater.getSeats(people);
                 // Check if the reservation can be accommodated for
                 if (allReservedSeats == null) {
-                    System.out.println("Not enough seats for reservation: " + reservation[0]);
+                    System.out.println("Not enough seats for reservation: " + reservation[IDENTIFIER_INDEX]);
                     continue;
                 }
                 // The reservation can be accommodated for
                 // Print all seating spots for this reservation
-                output.print(reservation[0] + " ");
+                output.print(reservation[IDENTIFIER_INDEX] + " ");
                 for (int i = 0; i < allReservedSeats.size(); i++) {
                     Integer[] reservedSeats = allReservedSeats.get(i);
-                    for (int j = reservedSeats[1]; j <= reservedSeats[2]; j++) {
-                        if (i != 0 || j != reservedSeats[1]) {
+                    for (int j = reservedSeats[STARTING_COLUMN_INDEX]; j <= reservedSeats[ENDING_COLUMN_INDEX]; j++) {
+                        if (i != 0 || j != reservedSeats[STARTING_COLUMN_INDEX]) {
                             output.print(",");
                         }
-                        output.print(intToRow.get(reservedSeats[0]));
+                        output.print(intToRow.get(reservedSeats[ROW_INDEX]));
                         output.print(j + 1);
                     }
                 }
